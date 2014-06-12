743591f46c11f87a16ad31e7f3090c9128c5ddda
Lukas Schauer <lukas@schauer.so>
2014-06-13 01:13:26 +0200

    arm_shell.sh

---
+++ emdebian/arm_shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "$(lsmod | grep binfmt_misc)" = "" ]; then
+  echo For this script you need to have binfmt_misc loaded.
+  exit 1
+fi
+
+docker run --privileged --rm -i -t -v "${DIR}"/..:/root/build lukas2511/emdebian bash -c 'update-binfmts --enable qemu-arm; bash || true; update-binfmts --disable qemu-arm'
