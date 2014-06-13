cfe55adfe4d907f1c15145d98526883aa6b5fc5d
Lukas Schauer <lukas@schauer.so>
2014-06-13 05:20:35 +0200

    added regeneration of ssh keys to firstboot-/setup-script

--- sdcard/debian/stuff/olinuxino_setup.sh
+++ sdcard/debian/stuff/olinuxino_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 if [ -f "/etc/.firstboot" ]; then
+  /bin/rm /etc/ssh/ssh_host_*
+  dpkg-reconfigure openssh-server
+  sync
   echo -e 'd \n n \n p \n 1 \n 2048 \n \n w \n' | fdisk /dev/mmcblk0
   rm /etc/.firstboot
   touch /etc/.secondboot
