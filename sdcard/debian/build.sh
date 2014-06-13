a994ebf9133fb81895225ac0f393aed7cfb78858
Lukas Schauer <lukas@schauer.so>
2014-06-13 06:39:44 +0200

    for now we need a privileged container for creating the sdcard images since we can't create loop devices otherwise, and i didn't find any other way yet

--- sdcard/debian/build.sh
+++ sdcard/debian/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run --rm -i -t -v "${DIR}"/../..:/root/build lukas2511/emdebian bash -c 'cd sdcard/debian; make'
+docker run --privileged --rm -i -t -v "${DIR}"/../..:/root/build lukas2511/emdebian bash -c 'cd sdcard/debian; make'
